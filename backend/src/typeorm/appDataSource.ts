a20b9ee87e222f10b9d7d1ce60e1adbe30a1177f
haus0409 <haconghau07@gmail.com>
2025-08-19 20:48:10 +0700

    sua lai env

--- backend/src/typeorm/appDataSource.ts
+++ backend/src/typeorm/appDataSource.ts
@@ -6,14 +6,15 @@ import { Cart } from "../entities/cart.entity";
 import { CartItems } from "../entities/cart-items.entity";
 import { Order } from "src/entities/order.entity";
 import { OrderItems } from "src/entities/order-items.entity";
+import "dotenv/config"
 
 export const postgresDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "lol009plo009",
-  database: "demothuctap3",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT)||5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [User, Role, Product, Cart, CartItems,Order,OrderItems],
   synchronize: true,
 });
